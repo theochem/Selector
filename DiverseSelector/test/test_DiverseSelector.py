049ac43504e1eab8dbc060c865a97e8076da3315
Fanwang Meng <fwmeng88@gmail.com>
2022-01-30 23:38:02 -0500

    Fix pylineter errors

--- DiverseSelector/test/test_DiverseSelector.py
+++ DiverseSelector/test/test_DiverseSelector.py
@@ -21,16 +21,15 @@
 #
 # --
 
-
 """
 Unit and regression test for the DiverseSelector package.
 """
 
-import pytest
-# Import package, test suite, and other packages as needed
 import sys
 
-import DiverseSelector
+
+# import pytest
+# import DiverseSelector
 
 
 def test_DiverseSelector_imported():
