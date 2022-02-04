98fd40bcfc24f52e8e126e79630957801d23f3c7
Fanwang Meng <fwmeng88@gmail.com>
2022-02-04 12:04:56 -0500

    Add molecular loader

--- DiverseSelector/utils.py
+++ DiverseSelector/utils.py
@@ -22,6 +22,9 @@
 # --
 
 """Utils module."""
+import gzip
+
+from rdkit import Chem
 
 __all__ = [
     "mol_reader",
@@ -30,8 +33,62 @@ __all__ = [
 ]
 
 
-def mol_reader():
-    """Load molecules as RDKit object."""
+def mol_reader(file_name: str,
+               remove_hydrogen: bool = False,
+               # force_field: str = None,
+               ) -> list:
+    """Load molecules as RDKit object.
+
+    Parameters
+    ----------
+    file_name : str
+        Input molecular file.
+    remove_hydrogen : bool, optional
+        To remove the hydrogen atoms. Default=False.
+
+    Returns
+    -------
+    mols: list
+        A list of RDKit molecule object.
+    """
+
+    # SDF
+    if file_name.lower().endswith(".sdf"):
+        suppl = Chem.SDMolSupplier(file_name, RemoveHs=remove_hydrogen, sanitize=True)
+        mols = [mol for mol in suppl]
+    # SDF.GZ
+    elif file_name.lower().endswith(".sdf.gz"):
+        file_unzipped = gzip.open(file_name)
+        suppl = Chem.ForwardSDMolSupplier(file_unzipped, RemoveHs=remove_hydrogen, sanitize=True)
+        mols = [mol for mol in suppl]
+    # SMILES: *.smi, *.smiles, *.txt, *.csv
+    elif file_name.lower().endswith((".smi", ".smiles", ".txt", ".csv")):
+        mols = []
+        with open(file_name, "r") as f:
+            for line in f:
+                mols.append(Chem.MolFromSmiles(line.strip()))
+
+    # todo:
+    # check if needed to add Hs
+    # generate 3D and minimize it with force field
+
+    # check if 3D or not
+    # with_3d_coord = True
+    # for mol in mols:
+    #     try:
+    #         AllChem.CalcPBF(mol)
+    #     except RuntimeError:
+    #         with_3d_coord = False
+    #         break
+
+    # wrap a private function which takes a molecule object as input
+    # 1. check if 3D coordinates
+    # 2. add Hs
+    # 3. generate 3D coordinates if needed
+    # then we can vectorize this function
+
+    return mols
+
     pass
 
 
