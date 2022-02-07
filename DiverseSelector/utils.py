f1fa796f92020b1b840117051df3e73b2b987a7d
Fanwang Meng <fwmeng88@gmail.com>
2022-02-06 22:01:34 -0500

    Add `get_features()` function in `utils.py`

--- DiverseSelector/utils.py
+++ DiverseSelector/utils.py
@@ -28,13 +28,20 @@ from typing import TypeVar
 import pandas as pd
 from rdkit import Chem
 
+from .feature import DescriptorGenerator, FingerprintGenerator
+
 __all__ = [
+    "ExplicitBitVect",
+    "RDKitMol",
+    "PandasDataFrame",
     "mol_reader",
     "feature_reader",
     "get_features",
 ]
 
 PandasDataFrame = TypeVar('pandas.core.frame.DataFrame')
+RDKitMol = TypeVar('rdkit.Chem.rdchem.Mol')
+ExplicitBitVect = TypeVar("rdkit.DataStructs.cDataStructs.ExplicitBitVect")
 
 
 def mol_reader(file_name: str,
@@ -132,23 +139,74 @@ def feature_reader(file_name: str,
     return df
 
 
-def get_features(mol_file,
-                 feature_file,
-                 ):
-    """Compute molecular features."""
-    # todo: can be refactored to run faster
+def get_features(sep: str = ",",
+                 engine: str = "python",
+                 feature_type=None,
+                 mol_file=None,
+                 feature_file=None,
+                 **kwargs,
+                 ) -> PandasDataFrame:
+    """Compute molecular features.
 
+    Parameters
+    ----------
+    sep : str, optional
+        Separator used for parsing feature file if text stle file provided. Default=",".
+    engine : str, optional
+        Separator used for parsing feature file with `pandas`. Default="python".
+    feature_type : str, optional
+        Feature type: "descriptor" or "fingerprint". Default=None.
+    mol_file : str, optional
+        Molecule file name. Default=None.
+    feature_file : str, optional
+        Feature file name. Default=None.
+    **kwargs:
+        Other keyword arguments.
+
+    Notes
+    -----
+    The user needs to specify at least one of `mol_file` and `feature_file`.
+
+    """
+    # todo: can be refactored to run faster
     # case: feature is not None, mol is None
     if mol_file is None and feature_file is not None:
-        features = feature_reader(feature_file)
+        df_features = feature_reader(feature_file, sep=sep, engine=engine)
     # case: feature is None, mol is not None
     elif mol_file is not None and feature_file is None:
-        features = feature_generator(mol_file)
+        mols = mol_reader(file_name=mol_file)
+        # compute descriptors
+        if feature_type.lower() == "descriptor":
+            descriptor_gen = DescriptorGenerator(mols, **kwargs)
+            df_features = descriptor_gen.compute_descriptor()
+        # compute fingerprints
+        elif feature_type.lower() == "fingerprint":
+            # todo: e3fp requires 3D coordinates
+            # todo: other fingerprints need 2D only
+            # change molecule 3D coordinate generation accordingly
+            fp_gen = FingerprintGenerator(mols, **kwargs)
+            df_features = fp_gen.compute_fingerprint()
+        else:
+            raise ValueError(f"{feature_type} is not supported.")
+
     # case: feature is not None, mol is not None
     elif mol_file is not None and feature_file is not None:
-        features = feature_reader(feature_file)
+        df_features = feature_reader(feature_file)
     # case: feature is None, mol is None
     else:
         raise ValueError("It is required to define the input molecule file or feature file.")
 
-    return features
+    return df_features
+
+
+def dump_mol():
+    """Save molecules."""
+    pass
+
+
+def dump_feature():
+    """Save selected molecule with features."""
+    pass
+
+# todo: dump_selected_index
+# todo: add index of selected molecules in base.py
