e880f959b662f0c04841aed3e284f2cc39af6d81
Hasan Khaleel <khaleeh@mcmaster.ca>
2022-02-17 10:43:05 -0500

    commit

--- DiverseSelector/metric.py
+++ DiverseSelector/metric.py
@@ -24,7 +24,7 @@
 """Metric calculation module."""
 
 import numpy as np
-from scipy.spatial.distance import cdist
+from scipy.spatial.distance import cdist, pdist
 
 __all__ = [
     "pairwise_dist",
@@ -36,20 +36,93 @@ def pairwise_dist(feature: np.array,
                   metric: str = "euclidean"):
     """Compute pairwise distance."""
     # more to be implemented
-    # https://docs.scipy.org/doc/scipy-1.8.0/html-scipyorg/reference/generated/
-    # scipy.spatial.distance.pdist.html?highlight=pdist#scipy.spatial.distance.pdist
+    # https://docs.scipy.org/doc/scipy-1.8.0/html-scipyorg/referenc
+    # e/generated/scipy.spatial.distance.pdist.html?highlight=pdist#scipy.spatial.distance.pdist
     if metric == "euclidean":
         arr_dist = cdist(feature, feature, "euclidean")
-
+        # per_dist = pdist(feature ,"euclidean")
     return arr_dist
 
 
-def compute_diversity():
-    """Compute the diversity."""
-    pass
+def euclidean(feature: np.array,
+              a_feature: np.array,
+              b_feature: np.array,
+              metric: str = "euclidean",):
+    """
+    Compute the euclidean distance
+    ----------
+    Parameters
+    ----------
+    a_feature : Features in molecule A 
+    b_feature : Features in molecule B
+    feature : number of features shared by both
+    -------
+    Returns
+    -------
+    distance : euclidean distance between molecule A & B
+    """
+    if metric == "euclidean":
+        if (a_feature) + (b_feature) - ((feature) * 2) > 0:
+            distance = np.sqrt((a_feature) + (b_feature) - ((feature) * 2))
+        else:
+            raise ValueError("Error in input")
+    return distance
+
+def soergel(feature: np.array,
+              a_feature: np.array,
+              b_feature: np.array,
+              metric: str = "soergel",):
+    """
+    Compute the soergel distance
+    ----------
+    Parameters
+    ----------
+    a_feature : Features in molecule A
+    b_feature : Features in molecule B
+    feature : number of features shared by both
+    -------
+    Returns
+    -------
+    distance : soergel distance between molecule A & B
+    """
+    if metric == "soergel":
+        if (a_feature + b_feature - feature) != 0:
+            distance = (a_feature + b_feature - (2 * feature)) / (a_feature + b_feature - feature)
+            return distance
+        else:
+            raise ValueError("Error in input")
+
+
+def compute_diversity(feature: np.array,
+                      a_feature: np.array,
+                      b_feature: np.array,
+                      metric: str = "tanimoto"):
+    """
+    Compute the diversity.
+    ----------
+    Parameters
+    ----------
+    a_feature : Features in molecule A
+    b_feature : Features in molecule B
+    feature : number of features shared by both
+    -------
+    Returns
+    -------
+    diversity : diversity between molecule A & B
+                1 being very diverse and 0 being not diverse
+    """
+    # diversity = 1 - similarity
+    # similarity = 1 / 1 + distance
+    if metric == "tanimoto":
+        if ((a_feature) + (b_feature) - (feature)) != 0:
+            similarity = (feature) /  (a_feature) + (b_feature) - (feature)
+            diversity = 1 - similarity
+            return diversity
+        else:
+            raise ValueError("Error in input")
 
 
 def total_diversity_volume():
     """Compute the total diversity volume."""
-
+    # V_tot = k * V_o
     pass
