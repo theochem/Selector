0c082a6ccceeb1495c72d84d17267ed8f60fe2a4
Fanwang Meng <fwmeng88@gmail.com>
2022-03-21 14:56:46 -0400

    Fix testing imports

--- DiverseSelector/test/__init__.py
+++ DiverseSelector/test/__init__.py
@@ -21,13 +21,4 @@
 #
 # --
 
-"""The main DiverseSelector Package."""
-
-from DiverseSelector.dissimilarity_based import DissimilaritySelection
-
-# Handle versioneer
-from ._version import get_versions
-versions = get_versions()
-__version__ = versions['version']
-__git_revision__ = versions['full-revisionid']
-del get_versions, versions
+"""The Test Module."""
