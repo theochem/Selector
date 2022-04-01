09789e5e5f890cb64697d7c5f024fd8b48ff1617
= <valerachuiko@gmail.com>
2022-03-31 21:39:15 -0400

    created DiverseSelector class and tests

--- DiverseSelector/clustering_based.py
+++ DiverseSelector/clustering_based.py
@@ -20,5 +20,118 @@
 # along with this program; if not, see <http://www.gnu.org/licenses/>
 #
 # --
+import numpy as np
+
+from DiverseSelector.base import SelectionBase
+from DiverseSelector.feature import get_features
+from DiverseSelector.metric import pairwise_dist
+from sklearn.cluster import KMeans, AffinityPropagation, MeanShift, SpectralClustering, AgglomerativeClustering, \
+    DBSCAN, OPTICS, Birch
+from sklearn.mixture import GaussianMixture
+
+__all__ = [
+    "ClusteringSelection",
+]
 
 """Clustering based compound selection."""
+
+
+class ClusteringSelection(SelectionBase):
+    def __init__(self,
+                 num_selected,
+                 num_clusters,
+                 clustering_method="k-means",
+                 metric="Tanimoto",
+                 feature_file=None,
+                 feature_type=None,
+                 mol_file=None,
+                 output=None,
+                 random_seed=None,
+                 arr_dist=None,
+                 **kwargs
+                 ):
+        """Base class for clustering based subset selection."""
+        super().__init__(metric, random_seed, feature_type, mol_file, feature_file, num_selected)
+        self.arr_dist = arr_dist
+        self.num_selected = num_selected
+        self.num_clusters = num_clusters
+
+        # the number of molecules equals the number of clusters
+        self.clustering_method = clustering_method
+        self.metric = metric
+        self.feature_file = feature_file
+        self.output = output
+        if arr_dist is None:
+            self.features = get_features(feature_file)
+        self.arr_dist = arr_dist
+        self.labels = None
+
+        if random_seed is None:
+            self.random_seed = 42
+        else:
+            self.random_seed = random_seed
+
+        self.__dict__.update(kwargs)
+
+        # check if number of clusters is less than number of selected molecules
+        if self.num_clusters > self.arr_dist.shape[0]:
+            raise ValueError("The number of clusters is great than number of molecules.")
+        # check if we have valid number of clusters because selecting 1.5 molecules is not practical
+        if int(self.num_selected / self.num_clusters) - self.num_selected / self.num_clusters != 0:
+            raise ValueError("The number of molecules in each cluster should be an integer.")
+
+        # check if selected number of clusters is less than the required number of molecules
+        if self.num_clusters > self.num_selected:
+            raise ValueError("The number of clusters cannot be greater than the number of "
+                             "selected molecules.")
+
+    def cluster(self, **params):
+        """
+        Performs clustering based on given algorithm from sklearn library and set of parameters
+        :param params: parameters for the selected method
+        :return: numpy array of labels: np.ndarray:
+        """
+        # todo: features
+        if self.clustering_method == 'k-means':
+            algorithm = KMeans(n_clusters=self.num_clusters, random_state=self.random_seed, **params).fit(self.features)
+        elif self.clustering_method == "affinity propagation":
+            algorithm = AffinityPropagation(random_state=self.random_seed, **params).fit(self.features)
+        elif self.clustering_method == 'mean shift':
+            algorithm = MeanShift(**params).fit(self.features)
+        elif self.clustering_method == 'spectral':
+            algorithm = SpectralClustering(n_clusters=self.num_clusters, **params).fit(self.features)
+        elif self.clustering_method == 'agglomerative':
+            algorithm = AgglomerativeClustering(n_clusters=self.num_clusters,
+                                                affinity=self.metric,
+                                                **params).fit(self.features)
+        elif self.clustering_method == 'DBSCAN':
+            algorithm = DBSCAN(metric=self.metric, **params).fit(self.features)
+        elif self.clustering_method == 'OPTICS':
+            algorithm = OPTICS(metric=self.metric, **params).fit(self.features)
+        elif self.clustering_method == 'birch':
+            algorithm = Birch(n_clusters=self.num_clusters, **params).fit(self.features)
+        elif self.clustering_method == "GMM":
+            labels = GaussianMixture(n_components=self.num_clusters, **params).fit_predict(self.features)
+            self.labels = labels
+            return labels
+        else:
+            raise ValueError("Clustering algorithm isn't supported")
+
+        if algorithm != 'GMM':
+            labels = algorithm.labels_
+            self.labels = labels
+
+    def select(self):
+        unique_labels = np.unique(self.labels)
+        n = self.num_selected//self.num_clusters
+
+        selected_all = []
+        for unique_label in unique_labels:
+            cluster_ids = np.where(self.labels == unique_label)[0]
+
+            np.random.seed(self.random_seed)
+            selected = np.random.choice(cluster_ids, size=n, replace=False)
+            selected_all.append(selected)
+
+        selected_all = np.array(selected_all).flatten()
+        return selected_all
