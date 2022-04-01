28c515f3c2d7eef7f3c6fa5337a6e808e90e31a5
Fanwang Meng <fwmeng88@gmail.com>
2022-04-01 17:15:48 -0400

    Add testing for RDKit descriptor

--- DiverseSelector/test/test_feature.py
+++ DiverseSelector/test/test_feature.py
@@ -96,3 +96,24 @@ def test_feature_desc_padelpy_3d():
     assert_almost_equal(df_padel_desc.to_numpy(float),
                         df_padel_desc_exp.to_numpy(float),
                         decimal=7)
+
+
+def test_feature_desc_rdkit():
+    """Testing molecular RDKit descriptor with 3d molecules."""
+    # load molecules
+    mols = load_testing_mols(mol_type="3d")
+    # generate molecular descriptors with the DescriptorGenerator
+    desc_generator = DescriptorGenerator(mols=mols,
+                                         desc_type="rdkit",
+                                         use_fragment=True,
+                                         ipc_avg=True,
+                                         )
+    df_rdkit_desc = desc_generator.compute_descriptor()
+    # load the expected descriptor dataframe
+    df_rdkit_desc_exp = pd.read_csv(os.path.join("data", "drug_mols_desc_rdkit.csv"),
+                                    sep=",")
+    # check if the dataframes are equal
+    assert_equal(df_rdkit_desc.shape, df_rdkit_desc_exp.shape)
+    assert_almost_equal(df_rdkit_desc.to_numpy(float),
+                        df_rdkit_desc_exp.to_numpy(float),
+                        decimal=7)
