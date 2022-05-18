04a73b52a65a57fa06800d8d69834278ed6d4529
Farnaz Heidar-Zadeh <farnaz_chem@yahoo.com>
2022-05-18 19:40:16 -0400

    Format all modules with black

--- DiverseSelector/__init__.py
+++ DiverseSelector/__init__.py
@@ -27,7 +27,8 @@ from DiverseSelector.Selectors import *
 
 # Handle versioneer
 from ._version import get_versions
+
 versions = get_versions()
-__version__ = versions['version']
-__git_revision__ = versions['full-revisionid']
+__version__ = versions["version"]
+__git_revision__ = versions["full-revisionid"]
 del get_versions, versions
