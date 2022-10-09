6741662633317391a14eb9319cae2f9ad1290520
Abdul Rahman Al Nabulsi <abdul.alnabulsi@gmail.com>
2022-10-09 05:29:28 -0400

    Fix linters

--- DiverseSelector/test/test_selectors.py
+++ DiverseSelector/test/test_selectors.py
@@ -23,8 +23,8 @@
 
 """Testing for the MaxMin selection algorithms."""
 
-from DiverseSelector.selectors import DirectedSphereExclusion, GridPartitioning, Medoid,\
-    MaxMin, MaxSum, OptiSim
+from DiverseSelector.selectors import DirectedSphereExclusion, GridPartitioning, MaxMin, MaxSum,\
+    Medoid, OptiSim
 from DiverseSelector.test.common import generate_synthetic_data
 import numpy as np
 from numpy.testing import assert_equal
