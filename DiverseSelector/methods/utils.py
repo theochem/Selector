f5766d19e7c6a30552ef3f87896baa42771c7c2b
awb9691 <abby.broscius@gmail.com>
2023-06-19 11:01:17 -0400

    add comments to DirectedSphereExclusion, predict_radius

--- DiverseSelector/methods/utils.py
+++ DiverseSelector/methods/utils.py
@@ -31,8 +31,7 @@ __all__ = [
 
 
 def predict_radius(obj, arr, num_selected, cluster_ids=None):
-    """
-    Algorithm that uses sphere_exclusion for selecting points from cluster.
+    """Algorithm that uses sphere_exclusion for selecting points from cluster.
 
     Parameters
     ----------
@@ -50,43 +49,56 @@ def predict_radius(obj, arr, num_selected, cluster_ids=None):
     selected: list
         List of ids of selected molecules
     """
+    # set the limits on # of selected points according to the tolerance percentage
     error = num_selected * obj.tolerance / 100
     lowlimit = num_selected - error
     uplimit = num_selected + error
-
+    # sort into clusters if data is labelled
     if cluster_ids is not None:
         arr = arr[cluster_ids]
 
     original_r = None
+    # use specified radius if passed in
     if obj.r is not None:
         original_r = obj.r
+        # run the selection algorithm
         result = obj.algorithm(arr, uplimit)
-    # Use numpy.optimize.bisect instead
+    # calculate radius from largest feature values
     else:
         rg = max(np.ptp(arr, axis=0)) / num_selected * 3
         obj.r = rg
         result = obj.algorithm(arr, uplimit)
+
+    # correct number of points chosen, return selected
     if len(result) == num_selected:
         return result
 
+    # incorrect number of points chosen
     low = obj.r if len(result) > num_selected else 0
     high = obj.r if low == 0 else None
     bounds = [low, high]
     count = 0
     while (len(result) < lowlimit or len(result) > uplimit) and count < 10:
+        # too many points selected, make radius larger
         if bounds[1] is None:
             rg = bounds[0] * 2
+        # too few points selected, make radius smaller
         else:
             rg = (bounds[0] + bounds[1]) / 2
         obj.r = rg
+        # re-run selection with new radius
         result = obj.algorithm(arr, uplimit)
+
+        # adjust upper/lower bounds of radius size to fine tune
         if len(result) > num_selected:
             bounds[0] = rg
         else:
             bounds[1] = rg
         count += 1
+    # cannot find radius that produces desired number of selected points
     if count >= 10:
         print(f"Optimal radius finder failed to converge, selected {len(result)} molecules instead "
               f"of requested {num_selected}.")
+    # undo any changes to radius
     obj.r = original_r
     return result
