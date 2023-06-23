fd0cac9a1e27e0e700e7439e3ec6c7b737a01fdd
AWBroscius <abby.broscius@gmail.com>
2023-06-23 17:16:18 -0400

    Remove dist_to_similarity(), and int passed as 'metric'

--- DiverseSelector/utils.py
+++ DiverseSelector/utils.py
@@ -29,40 +29,45 @@ from scipy.spatial.distance import squareform
 
 __all__ = [
     "sim_to_dist",
-    "distance_to_similarity",
     "reverse",
     "reciprocal",
     "exponential",
     "gaussian",
     "correlation",
     "transition",
-    "co_occur",
+    "co_occurrence",
     "gravity",
     "probability",
     "covariance",
 ]
 
+# todo: figure out how to handle divide by zero errors
+# affected functions: reciprocal,
+#                     exponential,
+#                     gaussian,
+#                     transition,
+#                     gravity,
+#                     probability
 
-def sim_to_dist(x, metric):
-    """Convert similarity coefficients to distance matrix.
+
+def sim_to_dist(x, metric: str):
+    """Convert similarity coefficients to distance array.
 
     Parameters
     ----------
     x : float or ndarray
         A similarity value as float, or a 1D or 2D array of similarity values.
         If 2D, the array is assumed to be symmetric.
-    metric : str, int
+    metric : str
         String or integer specifying which conversion metric to use.
         Supported metrics are "reverse", "reciprocal", "exponential",
         "gaussian", "membership", "correlation", "transition", "co-occurrence",
-        "gravity", "confusion", "probability", and "covariance". If the metric is an integer,
-        the similarities are subtracted from that integer and the result is
-        returned as distance.
+        "gravity", "confusion", "probability", and "covariance".
 
     Returns
     -------
-    ndarray :
-        Symmetric 2D distance array.
+    dist : float, ndarray
+         Distance value or array.
     """
     method_dict = {
         "reverse": reverse,
@@ -71,28 +76,35 @@ def sim_to_dist(x, metric):
         "gaussian": gaussian,
         "correlation": correlation,
         "transition": transition,
-        "co-occurrence": co_occur,
+        "co-occurrence": co_occurrence,
         "gravity": gravity,
         "probability": probability,
         "covariance": covariance,
     }
-    single = False
-    dist = None
-    # Convert all inputs to 2D array:
+
     # check if x is a single value
+    single = False
     if type(x) == float or type(x) == int:
         x = np.array([[x]])
         single = True
-    # check if x is 1D array
-    elif x.ndim == 1:
-        # todo: convert 1d sparse array to 2d similarity matrix
-        pass
 
-    # check that x was a valid input
+    # check if x is 1 array with invalid metric
+    if x.ndim == 1:
+        if metric == "co-occurrence" or metric == "gravity":
+            raise RuntimeError(
+                "This operation unsupported with 1D array. "
+                "Please make sure `x` is a 2D array when using the metric ",
+                metric,
+            )
+
+    # check that x is a valid array
     if x.ndim != 1 and x.ndim != 2:
-        raise ValueError(f"Attribute `x` should have dimension 1 or 2 rather than {x.ndim}.")
+        raise ValueError(
+            f"Attribute `x` should have dimension 1 or 2 rather than {x.ndim}."
+        )
 
     # call correct metric function
+    dist = None
     if type(metric) == str:
         if metric in method_dict:
             dist = method_dict[metric](x)
@@ -103,9 +115,6 @@ def sim_to_dist(x, metric):
                     "sure all input values are between [0, 1]."
                 )
             dist = 1 - x
-    # metric is an integer value
-    elif type(metric) == int:
-        dist = metric - x
     # unsupported metric
     else:
         raise ValueError(f"{metric} is an unsupported metric.")
@@ -118,7 +127,7 @@ def sim_to_dist(x, metric):
 
 
 def reverse(x: np.ndarray):
-    r"""Calculate distance matrix from similarity using the reverse method.
+    r"""Calculate distance array from similarity using the reverse method.
 
     .. math::
     \delta_{ij} = min(s_{ij}) + max(s_{ij}) - s_{ij}
@@ -126,17 +135,17 @@ def reverse(x: np.ndarray):
     where :math:`\delta_{ij}` is the distance between points :math:`i`
     and :math:`j`, :math:`s_{ij}` is their similarity coefficient,
     and :math:`max` and :math:`min` are the maximum and minimum
-    values across the entire similarity matrix.
+    values across the entire similarity array.
 
     Parameters
     -----------
     x : ndarray
-        Symmetric similarity array.
+        Similarity array.
 
     Returns
     -------
     ndarray :
-        Symmetric distance array.
+        Distance array.
 
     """
     dist = (np.max(x) + np.min(x)) - x
@@ -144,7 +153,7 @@ def reverse(x: np.ndarray):
 
 
 def reciprocal(x: np.ndarray):
-    r"""Calculate distance matrix from similarity using the reciprocal method.
+    r"""Calculate distance array from similarity using the reciprocal method.
 
     .. math::
     \delta_{ij} = \frac{1}{s_{ij}}
@@ -155,12 +164,12 @@ def reciprocal(x: np.ndarray):
     Parameters
     -----------
     x : ndarray
-        Symmetric similarity array.
+        Similarity array.
 
     Returns
     -------
     ndarray :
-        Symmetric distance array.
+        Distance array.
     """
 
     return 1 / x
@@ -178,12 +187,12 @@ def exponential(x: np.ndarray):
     Parameters
     -----------
     x : ndarray
-        Symmetric similarity array.
+        Similarity array.
 
     Returns
     -------
     ndarray :
-        Symmetric distance array.
+        Distance array.
 
     """
     y = x / (np.max(x))
@@ -203,12 +212,12 @@ def gaussian(x: np.ndarray):
     Parameters
     -----------
     x : ndarray
-        Symmetric similarity array.
+        Similarity array.
 
     Returns
     -------
     ndarray :
-        Symmetric distance array.
+        Distance array.
 
     """
     y = x / (np.max(x))
@@ -217,7 +226,7 @@ def gaussian(x: np.ndarray):
 
 
 def correlation(x: np.ndarray):
-    r"""Calculate distance matrix from correlation matrix.
+    r"""Calculate distance array from correlation array.
 
     .. math::
     \delta_{ij} = \sqrt{1 - r_{ij}}
@@ -228,12 +237,12 @@ def correlation(x: np.ndarray):
     Parameters
     -----------
     x : ndarray
-        Symmetric correlation array.
+        Correlation array.
 
     Returns
     -------
     ndarray :
-        Symmetric distance array.
+        Distance array.
 
     """
     if np.any(x < -1) or np.any(x > 1):
@@ -246,7 +255,7 @@ def correlation(x: np.ndarray):
 
 
 def transition(x: np.ndarray):
-    r"""Calculate distance matrix from frequency using the transition method.
+    r"""Calculate distance array from frequency using the transition method.
 
     .. math::
     \delta_{ij} = \frac{1}{\sqrt{f_{ij}}}
@@ -262,7 +271,7 @@ def transition(x: np.ndarray):
     Returns
     -------
     ndarray :
-        Symmetric distance array.
+        Distance array.
 
     """
     if np.any(x < 0):
@@ -279,8 +288,8 @@ def transition(x: np.ndarray):
     return dist
 
 
-def co_occur(x: np.ndarray):
-    r"""Calculate distance matrix from frequency using the co-occurrence method.
+def co_occurrence(x: np.ndarray):
+    r"""Calculate distance array from frequency using the co-occurrence method.
 
     .. math::
     \delta_{ij} =  \left(1 + \frac{f_{ij}\sum_{i,j}{f_{ij}}}
@@ -305,15 +314,15 @@ def co_occur(x: np.ndarray):
     j = np.sum(x, axis=1, keepdims=True)
     # multiply sums to scalar value
     bottom = np.dot(i, j)
-    # multiply each element by the sum of entire matrix
+    # multiply each element by the sum of entire array
     top = x * np.sum(x)
     # evaluate function as a whole
-    dist = (1 + (top/bottom))**-1
+    dist = (1 + (top / bottom)) ** -1
     return dist
 
 
 def gravity(x: np.ndarray):
-    r"""Calculate distance matrix from frequency using the gravity method.
+    r"""Calculate distance array from frequency using the gravity method.
 
     .. math::
         \delta_{ij} = \sqrt{\frac{\sum_{i}{f_{ij}}\sum_{j}{f_{ij}}}
@@ -338,15 +347,15 @@ def gravity(x: np.ndarray):
     j = np.sum(x, axis=1, keepdims=True)
     # multiply sums to scalar value
     top = np.dot(i, j)
-    # multiply each element by the sum of entire matrix
+    # multiply each element by the sum of entire array
     bottom = x * np.sum(x)
     # take square root of the fraction
-    dist = np.sqrt(top/bottom)
+    dist = np.sqrt(top / bottom)
     return dist
 
 
 def probability(x: np.ndarray):
-    r"""Calculate distance matrix from probability matrix.
+    r"""Calculate distance array from probability array.
 
     .. math::
     \delta_{ij} = \sqrt{-\ln{\frac{s_{ij}}{max(s_{ij})}}}
@@ -362,7 +371,7 @@ def probability(x: np.ndarray):
     Returns
     -------
     ndarray :
-        Symmetric distance array.
+        Distance array.
 
     """
     if np.any(x < 0) or np.any(x > 1):
@@ -376,7 +385,7 @@ def probability(x: np.ndarray):
 
 
 def covariance(x: np.ndarray):
-    r"""Calculate distance matrix from similarity using the covariance method.
+    r"""Calculate distance array from similarity using the covariance method.
 
     .. math::
     \delta_{ij} = \sqrt{s_{ii}+s_{jj}-2s_{ij}}
@@ -389,42 +398,19 @@ def covariance(x: np.ndarray):
     Parameters
     -----------
     x : ndarray
-        Symmetric covariance array.
+        Covariance array.
 
     Returns
     -------
     ndarray :
-        Symmetric distance array.
+        Distance array.
 
     """
-    # todo: optimize performance if possible
-    # initialize distance matrix
+    # initialize distance array
     dist = np.empty(x.shape)
     for i in range(0, x.shape[0]):
         for j in range(0, x.shape[1]):
-            # for each entry in covariance matrix, calculate distance
+            # for each entry in covariance array, calculate distance
             dist[i][j] = x[i][i] + x[j][j] - 2 * x[i][j]
     dist = np.sqrt(dist)
     return dist
-
-
-def distance_to_similarity(x: np.ndarray, dist: bool = True) -> np.ndarray:
-    """Convert between distance and similarity matrix.
-
-    Parameters
-    ----------
-    x : ndarray
-        Symmetric distance or similarity array.
-    dist : bool
-        Confirms the matrix is distance.
-
-    Returns
-    -------
-    y : ndarray
-        Symmetric distance or similarity array.
-    """
-    if dist is True:
-        y = 1 / (1 + x)
-    else:
-        y = (1 / x) - 1
-    return y
