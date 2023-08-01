a5bd9874e4f184f6f58ecc672ae9ff175adbd9de
maximilianvz <max.p.vanzyl@gmail.com>
2023-08-01 16:23:31 -0400

    Polish documentation and variable names

--- DiverseSelector/methods/dissimilarity.py
+++ DiverseSelector/methods/dissimilarity.py
@@ -36,16 +36,22 @@ __all__ = [
 
 
 class MaxMin(SelectionBase):
-    """Selecting compounds using MaxMin algorithm.
+    """Selecting samples using MaxMin algorithm.
 
-    Initial point is chosen as medoid center. The second point is
-    the furthest point away. All the following points are selected
-    using the rule:
-    1. Find minimum distance from every point to the selected ones.
-    2. Select a point the has the maximum distance among calculated
-       on the previous step.
+    MaxMin is possibly the most widely used method for dissimilarity-based
+    compound selection. When presented with a dataset of samples, the
+    initial point is chosen as the dataset's medoid center. Next, the second
+    point is chosen to be that which is furthest from this initial point.
+    Subsequently, all following points are selected via the following
+    logic:
 
-    The algorithm is described well here: https://doi.org/10.1002/qsar.200290002
+    1. Find the minimum distance from every point to the already-selected ones.
+    2. Select the point which has the maximum distance among those calculated
+       in the previous step.
+
+    Notes
+    -----
+    The algorithm is described well here: Quant. Struct.-Act. Relat., 21 (2002). https://doi.org/10.1002/qsar.200290002
     """
 
     def __init__(self, func_distance=None):
@@ -55,47 +61,46 @@ class MaxMin(SelectionBase):
         Parameters
         ----------
         func_distance: callable
-            Function for calculating the pairwise distance between instances of the array.
+            Function for calculating the pairwise distance between instances of the coordinates array.
         """
         self.func_distance = func_distance
 
-    def select_from_cluster(self, arr, num_selected, cluster_ids=None):
-        """
-        Algorithm MaxMin for selecting points from cluster.
+    def select_from_cluster(self, X, size, cluster_ids=None):
+        """Return selected samples from a cluster based on MaxMin algorithm.
 
         Parameters
         ----------
-        arr: np.ndarray
+        X: np.ndarray
             Distance matrix for points that needs to be selected if func_distance is None.
             Otherwise, treated as coordinates array.
-        num_selected: int
-            Number of molecules that need to be selected
+        size: int
+            Number of samples to be selected.
         cluster_ids: np.ndarray
-            Indices of molecules that form a cluster
+            Indices of samples that form a cluster.
 
         Returns
         -------
         selected: list
-            List of ids of selected molecules
+            List of indices of selected samples.
         """
         if self.func_distance is not None:
             # calculate pairwise distance between instances of provided array
-            arr_dist = self.func_distance(arr)
+            X_dist = self.func_distance(X)
         else:
             # assume provided array already describes pairwise distances between samples
-            arr_dist = arr
+            X_dist = X
 
         if cluster_ids is not None:
             # extract pairwise distances from full pairwise distance matrix to obtain a new matrix that only contains
             # pairwise distances between samples within a given cluster
-            arr_dist = arr_dist[cluster_ids][:, cluster_ids]
+            X_dist = X_dist[cluster_ids][:, cluster_ids]
 
         # choosing initial point as the medoid (i.e., point with minimum cumulative pairwise distances to other points)
-        selected = [np.argmin(np.sum(arr_dist, axis=0))]
+        selected = [np.argmin(np.sum(X_dist, axis=0))]
         # select following points until desired number of points have been obtained
-        while len(selected) < num_selected:
+        while len(selected) < size:
             # determine the minimum pairwise distances between the selected points and all the other points
-            min_distances = np.min(arr_dist[selected], axis=0)
+            min_distances = np.min(X_dist[selected], axis=0)
             # determine which point affords the maximum distance among the minimum distances captured in min_distances
             new_id = np.argmax(min_distances)
             selected.append(new_id)
@@ -103,14 +108,24 @@ class MaxMin(SelectionBase):
 
 
 class MaxSum(SelectionBase):
-    """Selecting compounds using MaxSum algorithm.
+    """Selecting samples using MaxSum algorithm.
 
-    Initial point is chosen as medoid center. The second point is
-    the furthest point away. All the following points are selected
-    using the rule:
-    1. Determine the sum of distances from every point to the selected ones.
-    2. Select a point the has the maximum sum of distance among calculated
-       on the previous step.
+    Whereas the goal of the MaxMin algorithm is to maximize the minimum distance
+    between any pair of distinct elements in the selected subset of a dataset,
+    the MaxSum algorithm aims to maximize the sum of distances between all
+    pairs of elements in the selected subset. When presented with a dataset of
+    samples, the initial point is chosen as the dataset's medoid center. Next,
+    the second point is chosen to be that which is furthest from this initial
+    point. Subsequently, all following points are selected via the following
+    logic:
+
+    1. Determine the sum of distances from every point to the already-selected ones.
+    2. Select the point which has the maximum sum of distances among those calculated
+       in the previous step.
+
+    Notes
+    -----
+    The following may be of use for further reading: https://doi.org/10.48550/arXiv.1203.6397
     """
 
     def __init__(self, func_distance=None):
@@ -120,51 +135,50 @@ class MaxSum(SelectionBase):
         Parameters
         ----------
         func_distance: callable
-            Function for calculating the pairwise distance between instances of the array.
+            Function for calculating the pairwise distance between instances of the coordinates array.
         """
         self.func_distance = func_distance
 
-    def select_from_cluster(self, arr, num_selected, cluster_ids=None):
-        """
-        Algorithm MaxSum for selecting points from cluster.
+    def select_from_cluster(self, X, size, cluster_ids=None):
+        """Return selected samples from a cluster based on MaxSum algorithm.
 
         Parameters
         ----------
-        arr: np.ndarray
+        X: np.ndarray
             Distance matrix for points that needs to be selected if func_distance is None.
             Otherwise, treated as coordinates array.
-        num_selected: int
-            Number of molecules that need to be selected
+        size: int
+            Number of samples to be selected.
         cluster_ids: np.ndarray
-            Indices of molecules that form a cluster
+            Indices of samples that form a cluster.
 
         Returns
         -------
         selected: list
-            List of ids of selected molecules
+            List of indices of selected samples.
         """
-        if num_selected > len(arr):
-            raise ValueError(f"Requested {num_selected} points which is greater than {len(arr)} "
+        if size > len(X):
+            raise ValueError(f"Requested {size} points which is greater than {len(X)} "
                              f"points provided in array")
 
         if self.func_distance is not None:
             # calculate pairwise distance between instances of provided array
-            arr_dist = self.func_distance(arr)
+            X_dist = self.func_distance(X)
         else:
             # assume provided array already describes pairwise distances between samples
-            arr_dist = arr
+            X_dist = X
 
         if cluster_ids is not None:
             # extract pairwise distances from full pairwise distance matrix to obtain a new matrix that only contains
             # pairwise distances between samples within a given cluster
-            arr_dist = arr_dist[cluster_ids][:, cluster_ids]
+            X_dist = X_dist[cluster_ids][:, cluster_ids]
 
         # choosing initial point as the medoid (i.e., point with minimum cumulative pairwise distances to other points)
-        selected = [np.argmin(np.sum(arr_dist, axis=0))]
+        selected = [np.argmin(np.sum(X_dist, axis=0))]
         # select following points until desired number of points have been obtained
-        while len(selected) < num_selected:
+        while len(selected) < size:
             # determine sum of pairwise distances between selected points and all other points
-            sum_distances = np.sum(arr_dist[selected], axis=0)
+            sum_distances = np.sum(X_dist[selected], axis=0)
             while True:
                 # determine which new point has the maximum sum of pairwise distances with already-selected points
                 new_id = np.argmax(sum_distances)
@@ -177,30 +191,32 @@ class MaxSum(SelectionBase):
 
 
 class OptiSim(SelectionBase):
-    """Selecting compounds using OptiSim algorithm.
+    """Selecting samples using OptiSim algorithm.
 
-    Initial point is chosen as medoid center. Points are randomly chosen and added to a subsample
-    if outside of radius r from all previously selected points, and discarded otherwise. Once k
-    number of points are added to the subsample, the point with the greatest minimum distance to
-    previously selected points is selected and the subsample is cleared and the process repeats.
+    The OptiSim algorithm selects samples from a dataset by first choosing the medoid center as the
+    initial point. Next, points are randomly chosen and added to a subsample if they exist
+    outside of radius r from all previously selected points (otherwise, they are discarded). Once k
+    number of points have been added to the subsample, the point with the greatest minimum distance to
+    the previously selected points is chosen. Then, the subsample is cleared and the process is repeated.
 
-    Adapted from  https://doi.org/10.1021/ci970282v
+    Notes
+    -----
+    J. Chem. Inf. Comput. Sci. 1997, 37, 6, 1181–1188. https://doi.org/10.1021/ci970282v
     """
 
-    def __init__(self, r=None, k=10, tol=5.0, eps=0, p=2, start_id=0, random_seed=42, n_iter=10):
-        """
-        Initializing class.
+    def __init__(self, r0=None, k=10, tol=5.0, eps=0, p=2, start_id=0, random_seed=42, n_iter=10):
+        """Initialize class.
 
         Parameters
         ----------
-        r: float
-            Initial guess of radius for optisim algorithm, no points within r distance to an already
+        r0: float
+            Initial guess of radius for OptiSim algorithm. No points within r distance to an already
             selected point can be selected.
         k: int
             Amount of points to add to subsample before selecting one of the points with the
             greatest minimum distance to the previously selected points.
         tol: float
-            Percentage error of number of molecules actually selected from number of molecules
+            Percentage error of number of samples actually selected from number of samples
             requested.
         eps: float
             Approximate nearest neighbor search for eliminating close points. Branches of the tree
@@ -216,7 +232,7 @@ class OptiSim(SelectionBase):
         n_iter: int
             Number of iterations to execute when optimizing the size of exclusion radius. Default is 10.
         """
-        self.r = r
+        self.r = r0
         self.k = k
         self.tol = tol
         self.eps = eps
@@ -225,36 +241,35 @@ class OptiSim(SelectionBase):
         self.random_seed = random_seed
         self.n_iter = n_iter
 
-    def algorithm(self, arr, uplimit) -> list:
-        """
-        Optisim algorithm logic.
+    def algorithm(self, X, max_size) -> list:
+        """Return selected samples based on OptiSim algorithm.
 
         Parameters
         ----------
-        arr: np.ndarray
-            Coordinate array of points.
-        uplimit: int
-            Maximum number of points to select.
+        X: np.ndarray
+            Coordinate array of samples.
+        max_size: int
+            Maximum number of samples to select.
 
         Returns
         -------
         selected: list
-            List of ids of selected molecules
+            List of indices of selected sample.
         """
         selected = [self.start_id]
         count = 1
 
         # establish a kd-tree for nearest-neighbor lookup
-        tree = spatial.KDTree(arr)
+        tree = spatial.KDTree(X)
         # use a random number generator that will be used to randomly select points
         rng = np.random.default_rng(seed=self.random_seed)
 
-        len_arr = len(arr)
+        len_X = len(X)
         # bv will serve as a mask to discard points within radius r of previously selected points
-        bv = np.zeros(len_arr)
-        candidates = list(range(len_arr))
+        bv = np.zeros(len_X)
+        candidates = list(range(len_X))
         # determine which points are within radius r of initial point
-        elim = tree.query_ball_point(arr[self.start_id], self.r, eps=self.eps, p=self.p, workers=-1)
+        elim = tree.query_ball_point(X[self.start_id], self.r, eps=self.eps, p=self.p, workers=-1)
         # exclude points within radius r of initial point from list of candidates using bv mask
         for idx in elim:
             bv[idx] = 1
@@ -269,45 +284,44 @@ class OptiSim(SelectionBase):
                 sublist = candidates.compressed()
 
             # create a new kd-tree for nearest neighbor lookup with candidates
-            newtree = spatial.KDTree(arr[selected])
+            newtree = spatial.KDTree(X[selected])
             # query the kd-tree for nearest neighbors to selected samples
-            search, _ = newtree.query(arr[sublist], eps=self.eps, p=self.p, workers=-1)
+            search, _ = newtree.query(X[sublist], eps=self.eps, p=self.p, workers=-1)
             # identify the nearest neighbor with the largest distance from previously selected samples
             search_idx = np.argmax(search)
             best_idx = sublist[search_idx]
             selected.append(best_idx)
 
             count += 1
-            if count > uplimit:
+            if count > max_size:
                 # do this if you have reached the maximum number of points selected
                 return selected
 
             # eliminate all remaining candidates within radius r of the sample that was just selected
-            elim = tree.query_ball_point(arr[best_idx], self.r, eps=self.eps, p=self.p, workers=-1)
+            elim = tree.query_ball_point(X[best_idx], self.r, eps=self.eps, p=self.p, workers=-1)
             for idx in elim:
                 bv[idx] = 1
             candidates = np.ma.array(candidates, mask=bv)
 
         return selected
 
-    def select_from_cluster(self, arr, num_selected, cluster_ids=None):
-        """
-        Algorithm that uses optisim for selecting points from cluster.
+    def select_from_cluster(self, X, size, cluster_ids=None):
+        """Return selected samples from a cluster based on OptiSim algorithm.
 
         Parameters
         ----------
-        arr: np.ndarray
-            Coordinate array of points
-        num_selected: int
-            Number of molecules that need to be selected.
+        X: np.ndarray
+            Coordinate array of samples.
+        size: int
+            Number of samples to be selected.
         cluster_ids: np.ndarray
-            Indices of molecules that form a cluster
+            Indices of samples that form a cluster.
 
         Returns
         -------
         selected: list
-            List of ids of selected molecules
+            List of indices of selected samples.
         """
-        return optimize_radius(self, arr, num_selected, cluster_ids)
+        return optimize_radius(self, X, size, cluster_ids)
 
 
