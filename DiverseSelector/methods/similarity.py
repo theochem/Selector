72cede2873de71de4555b1fb65674a2af9e25e7e
Marco Martínez González <mmg870630@gmail.com>
2023-10-12 02:09:53 -0400

    Clean __call__ method

--- DiverseSelector/methods/similarity.py
+++ DiverseSelector/methods/similarity.py
@@ -471,11 +471,7 @@ class SimilarityIndex:
         self.w_factor = w_factor
         self.c_threshold = c_threshold
 
-    def _calculate_counters(
-        self,
-        arr: np.ndarray,
-        n_objects: Optional[int] = None,
-    ) -> dict:
+    def _calculate_counters(self, arr: np.ndarray, n_objects: Optional[int] = None) -> dict:
         """Calculate 1-similarity, 0-similarity, and dissimilarity counters.
 
         Arguments
@@ -614,14 +610,7 @@ class SimilarityIndex:
         }
         return counters
 
-    def __call__(
-        self,
-        arr: np.ndarray = None,
-        n_objects: int = None,
-        similarity_index: str = None,
-        c_threshold: Union[None, str, int] = None,
-        w_factor: str = None,
-    ) -> float:
+    def __call__(self, arr: np.ndarray, n_objects: int = None) -> float:
         """Calculate the similarity index of a set of vectors.
 
         Parameters
@@ -635,78 +624,15 @@ class SimilarityIndex:
             Number of objects in the data. Is only necessary if the data is a columnwise sum of
             the objects. If the data is not the columnwise sum of the objects, the number of objects
             is calculated as the length of the data.
-        similarity_index: string
-            Key with the abbreviation of the desired similarity index to be calculated for the data.
-            Possible values are:
-                AC: Austin-Colwell
-                BUB: Baroni-Urbani-Buser
-                CTn: Consoni-Todschini
-                Fai: Faith
-                Gle: Gleason
-                Ja: Jaccard
-                Ja0: Jaccard 0-variant
-                JT: Jaccard-Tanimoto
-                RT: Rogers-Tanimoto
-                RR: Russel-Rao
-                SM: Sokal-Michener
-                SSn: Sokal-Sneath n
-        c_threshold: {None, 'dissimilar', int}
-            Coincidence threshold used for calculating the similarity counters. A position of the
-            elements is considered to be a coincidence (coincides among all the elements considered)
-            if the number of elements that have the same value in that position is greater than the
-            coincidence threshold.
-            arrays
-                None : Default, c_threshold = n_objects % 2
-                'dissimilar' : c_threshold = ceil(n_objects / 2)
-                int : Integer number < n_objects
-        w_factor: {"fraction", "power_n"}
-            Type of weight function that will be used.
-            'fraction' : similarity = d[k]/n
-                            dissimilarity = 1 - (d[k] - n_objects % 2)/n_objects
-            'power_n' : similarity = n**-(n_objects - d[k])
-                        dissimilarity = n**-(d[k] - n_objects % 2)
-            other values : similarity = dissimilarity = 1
-
         Returns
         -------
         similarity_index: float
             Similarity index of the set of vectors.
         """
-        # If the parameters are not provided, the parameters provided in the class initialization
-        # are used. If the parameters are provided, the parameters values are checked and used.
-        if similarity_index is None:
-            similarity_index = self.similarity_index
-        else:
-            # check if the similarity index is valid
-            if similarity_index not in _similarity_index_dict:
-                raise ValueError(
-                    f'Similarity index "{similarity_index}" is not available. '
-                    f"See the documentation for the available similarity indexes."
-                )
-        if w_factor is None:
-            w_factor = self.w_factor
-        else:
-            # check if the w_factor is valid
-            if w_factor not in ["fraction", "power_n"]:
-                print(
-                    f'Weight factor "{w_factor}" given. Using default value '
-                    '"similarity = dissimilarity = 1".'
-                )
-        if c_threshold is None:
-            c_threshold = self.c_threshold
-        else:
-            # check if the c_threshold is valid
-            if c_threshold not in ["dissimilar", None]:
-                if not isinstance(c_threshold, int):
-                    raise ValueError(
-                        f'Invalid c_threshold. It must be an integer or "dissimilar" or None. '
-                        f"Given c_threshold = {c_threshold}"
-                    )
-
         # check that data or c_total is provided
         if arr is None:
             raise ValueError("Please provide data or c_total")
-        # check if data is a np.ndarray
+        # check if arr is a np.ndarray
         if not isinstance(arr, np.ndarray):
             raise TypeError("Input data is not a np.ndarray, please input the right data type")
 
@@ -724,11 +650,9 @@ class SimilarityIndex:
             n_objects = arr.shape[0]
 
         # calculate the counters needed to calculate the similarity indexes
-        counters = self._calculate_counters(
-            arr=c_total, n_objects=n_objects, w_factor=w_factor, c_threshold=c_threshold
-        )
+        counters = self._calculate_counters(arr=c_total, n_objects=n_objects)
         # calculate the similarity index
-        similarity_index = _similarity_index_dict[similarity_index](counters)
+        similarity_index = _similarity_index_dict[self.similarity_index](counters)
 
         return similarity_index
 
@@ -778,17 +702,15 @@ class SimilarityIndex:
                         dissimilarity = n**-(d[k] - n_objects % 2)
             other values : similarity = dissimilarity = 1
         """
-        if arr is None:
-            raise ValueError("Please provide data or c_total")
         # Check if the data is a np.ndarray of a list
         if not isinstance(arr, np.ndarray):
             raise TypeError("Input data is not a np.ndarray, please input the right data type")
-        # Check if data is one dimensional
+        # Check if the data is one dimensional
         if arr.ndim != 2:
             raise ValueError(
                 "Data must be a two dimensional np.ndarray for calculating the medoid."
             )
-        # Check if data has at least 3 rows
+        # Check if the data has at least 3 rows
         if arr.shape[0] < 3:
             raise ValueError("Input data must have at least 3 rows to calculate the medoid.")
         # If the parameters are not provided, the parameters provided in the class initialization
@@ -912,20 +834,18 @@ class SimilarityIndex:
                         dissimilarity = n**-(d[k] - n_objects % 2)
             other values : similarity = dissimilarity = 1
         """
-        # check if data is provided
-        if arr is None:
-            raise ValueError("Please provide data or c_total")
         # Check if the data is a np.ndarray of a list
         if not isinstance(arr, np.ndarray):
             raise TypeError("Input data is not a np.ndarray, please input the right data type")
-        # Check if data is one dimensional
+        # Check if the data is one dimensional
         if arr.ndim != 2:
             raise ValueError(
                 "Data must be a two dimensional np.ndarray for calculating the outlier."
             )
-        # Check if data has at least 3 rows
+        # Check if the data has at least 3 rows
         if arr.shape[0] < 3:
             raise ValueError("Input data must have at least 3 rows to calculate the outlier.")
+
         # If the parameters are not provided, the parameters provided in the class initialization
         # are used. If the parameters are provided, the parameters values are checked and used.
         if similarity_index is None:
