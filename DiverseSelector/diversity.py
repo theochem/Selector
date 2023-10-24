6e736ecd66b848a5e94c98b3d47be52870e11c30
Fanwang Meng <fwmeng@mit.edu>
2023-10-24 16:11:51 -0400

    Remove the `entropy()` function

--- DiverseSelector/diversity.py
+++ DiverseSelector/diversity.py
@@ -112,58 +112,6 @@ def compute_diversity(
         raise ValueError(f"Diversity type {div_type} not supported.")
 
 
-def entropy(x: np.ndarray) -> float:
-    r"""Compute entropy of matrix.
-
-    The equation for entropy is
-    .. math::
-        E = -\frac{\sum{\frac{y_i}{N}\ln{\frac{y_i}{N}}}}{L\frac{\ln{2}}{2}}
-
-    where :math:`N` is the number of molecules in the set, :math:`L` is the length of the
-    fingerprint, and :math:`y_i` is a vector of the bit-counts of each feature in the fingerprints.
-
-    Higher values mean more diversity.
-
-    Parameters
-    ----------
-    x : ndarray
-        Feature matrix.
-
-    Returns
-    -------
-    e : float
-        Entropy of matrix in the range [0,1].
-
-    Notes
-    -----
-    The feature matrix does not need to be in bitstring form to use this function.
-    However, the features should be encoded in a binary way, such that 0 means
-    absence of the feature, and nonzero means presence of the feature.
-
-    References
-    -----------
-    Weidlich, I. E., and Filippov, I. V., Using the Gini coefficient to measure the chemical
-    diversity of small-molecule libraries, Journal of Computational Chemistry, (2016) 37, 2091-2097.
-
-    """
-
-    # initialize variables
-
-    num_features = len(x[0])
-    num_points = len(x)
-    top = 0
-    # count bits in fingerprint and order them
-    counts = np.count_nonzero(x, axis=0)
-    counts = np.sort(counts)
-    # sum entropy calculation for each feature
-    if 0 in counts:
-        raise ValueError("Redundant feature in matrix. Please remove it and try again.")
-    for i in range(0, num_features):
-        top += ((counts[i]) / num_points) * (np.log(counts[i] / num_points))
-    ent = -1 * (top / (num_features * 0.34657359027997264))
-    return ent
-
-
 def logdet(x: np.ndarray) -> float:
     r"""Computes the log determinant function.
 
@@ -206,6 +154,11 @@ def shannon_entropy(x: np.ndarray, normalize=True, truncation=False) -> float:
     ----------
     x : ndarray
         Bit-string matrix.
+    normalize : bool, optional
+        Normalize the entropy to [0, 1]. Default=True.
+    truncation : bool, optional
+        Use the truncated Shannon entropy by only counting the contributions of one-bits.
+        Default=False.
 
     Returns
     -------
@@ -361,7 +314,7 @@ def wdud(x: np.ndarray) -> float:
     min_x = np.min(x, axis=0)
     # Normalization of each feature to [0, 1]
     if np.any(np.abs(max_x - min_x) < 1e-30):
-        raise ValueError(f"One of the features is redundant and causes normalization to fail.")
+        raise ValueError("One of the features is redundant and causes normalization to fail.")
     x_norm = (x - min_x) / (max_x - min_x)
     ans = []  # store the Wasserstein distance for each feature
     for i in range(0, num_features):
