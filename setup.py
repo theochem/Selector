5fc4f62cf94816e937905db8b03b5a5413da00b1
kunikachandra <kunikachandra6@gmail.com>
2024-04-16 21:55:29 +0530

    Support dynamic naming of pypi packages using setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ import sys
 
 from setuptools import setup
 
-short_description = "Subset selection with maximized diversity".split("\n")[0]
+short_description = "Molecule selection with maximum diversity".split("\n")[0]
 
 # from https://github.com/pytest-dev/pytest-runner#conditional-requirement
 needs_pytest = {"pytest", "test", "ptr"}.intersection(sys.argv)
@@ -37,8 +37,7 @@ try:
 except ValueError:
     long_description = short_description
 
-
-setup(
+package_metadata = dict(
     name="selector",
     author="QC-Devs Community",
     author_email="qcdevs@gmail.com",
@@ -46,7 +45,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     version="0.0.1",
-    license="GNU (Version 3)",
+    license="GNU General Public License v3 (GPLv3)",
     package_dir={"selector": "selector"},
     packages=["selector", "selector.methods", "selector.tests", "selector.methods.tests"],
     # Optional include package data to ship with your package
@@ -82,3 +81,17 @@ setup(
     # zip_safe=False,
     # todo: add classifiers
 )
+
+# Naming format of the pypi wheel
+wheel_name_format = "{name}-{version}-{py_version}-none-any.whl"
+
+# Modify wheel file name based on the platform and Python version
+distname = wheel_name_format.format(
+    name=package_metadata["name"],
+    version=package_metadata["version"],
+    py_version="py" + "".join(map(str, sys.version_info[:2]))
+)
+
+package_metadata["distname"] = distname
+
+setup(**package_metadata)
